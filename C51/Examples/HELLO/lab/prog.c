011df23877b820c0336e7524b82fd48b221b4c2e
TanmayVig <tanmayvij11@gmail.com>
2022-01-30 21:23:48 +0530

    ES Lab 2 Submission

---
+++ C51/Examples/HELLO/lab/prog.c
@@ -0,0 +1,17 @@
+#include<p89v51rd2.h>
+
+void delay(unsigned int d){
+	unsigned int i,j;
+	for(i=0;i<=1000;i++){
+		for(j=0;j<=d;j++);
+	}
+}
+
+void main(void){
+	while(1){
+		RxD=0;
+		delay(20);
+		RxD=1;
+		delay(20);
+	}
+}
